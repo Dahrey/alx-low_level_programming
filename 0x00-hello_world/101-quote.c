7b6194bc67e570faa1971b1bd86b4ee18f6161c6
Dahrey <damdre03@gmail.com>
2023-08-10 21:36:45 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
-* main - A program that print
-* Return: 1 (Pass)
-*/
+ *  main - A c program
+ *  Return 1 (pass)
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
